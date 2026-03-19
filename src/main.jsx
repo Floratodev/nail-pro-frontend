a2c8506c892a933d3b5095213a9cf1ed9454adc2
Floratodev <florato.dev@gmail.com>
2026-03-19 23:33:52 +0100

    install recharts to fix import error in App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'          // si no existe, créalo vacío o bórralo esta línea
+// import './index.css'  // comenta si no existe o da error
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
